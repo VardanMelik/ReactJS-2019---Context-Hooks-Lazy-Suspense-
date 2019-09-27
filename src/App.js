fa9bf1cdf17133948181d14a296ea8ab5a484fdf
eyemotion <eyemotiongamestudio@gmail.com>
2019-09-27 15:49:48 +0400

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import TodoList from './Todo/TodoList'
 
 
 function App() {
+
+  const todos = [
+    {id: 1, completed: false, title: 'Buy Bread'},
+    {id: 2, completed: false, title: 'Buy Car'},
+    {id: 3, completed: false, title: 'Buy House'},
+    {id: 4, completed: false, title: 'Buy Mouse'}
+  ]
     return (
       <div className="wrapper">
         <h1>React</h1>
-        <TodoList />
+        <TodoList todos={todos} />
       </div>
     );
 }
